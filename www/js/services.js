4e62d2643fa467ece73cb20cc7ccf800d9cde08e
imagine21 <joanasimoes90@gmail.com>
2016-11-02 17:18:08 +0000

    changes from ionic lists exercise, week 2

--- www/js/services.js
+++ www/js/services.js
@@ -45,4 +45,31 @@ angular.module('conFusion.services', ['ngResource'])
 
         }])
 
+        .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+          var favFac = {};
+          var favorites = [];
+
+          favFac.addToFavorites = function (index) {
+            for (var i = 0; i < favorites.length; i++) {
+              if (favorites[i].id == index)
+                return;                               //this step is to make sure there are no duplicates in the list
+            }
+            favorites.push({id: index});   //is a js object
+          };
+
+          favFac.deleteFromFavorites = function (index) {
+            for (var i = 0; i < favorites.length; i++) {
+              if (favorites[i].id == index) {
+                favorites.splice(i, 1);
+              }
+            }
+          }
+
+          favFac.getFavorites = function () {
+            return favorites;
+          };
+
+          return favFac;
+        }])
+
 ;
