6b8cf295b66dbc5ad3f84f421dee830fe305abe3
imagine21 <joanasimoes90@gmail.com>
2016-11-08 14:52:19 +0000

    made use of the HTML5 local storage in the application, first by creating a service to wrap the local storage, and then injected into a controller in order to use the storage

--- www/js/services.js
+++ www/js/services.js
@@ -12,7 +12,7 @@ angular.module('conFusion.services', ['ngResource'])
 
   }])
 
-  
+
   .factory('promotionFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
     return $resource(baseURL + "promotions/:id");
 
@@ -59,5 +59,24 @@ angular.module('conFusion.services', ['ngResource'])
 
     return favFac;
   }])
+
+
+  .factory('$localStorage', ['$window', function($window) {
+    return {
+      store: function(key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function(key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+      },
+      storeObject: function(key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject: function(key,defaultValue) {
+        return JSON.parse($window.localStorage[key] || defaultValue);
+      }
+    }
+  }])
+
 
 ;
